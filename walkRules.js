8c6c72116aca3cb83323aa79a55c31af0b4b355e
Lea Verou <lea@verou.me>
2020-08-31 19:22:02 +0300

    Import rework-utils from css-almanac

---
+++ walkRules.js
@@ -0,0 +1,16 @@
+// Recursively walk all "normal" rules, i.e. rules with selectors
+export default function walkRules(rules, callback, test) {
+	if (!rules) {
+		return;
+	}
+
+	for (let rule of rules) {
+		if (!test || test(rule)) {
+			callback(rule);
+		}
+
+		if (rule.rules) {
+			walkRules(rule.rules, callback);
+		}
+	}
+}
