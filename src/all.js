80f95069872253ab87d0caeef4e1a34ee91cc353
Lea Verou <lea@verou.me>
2020-09-05 19:13:27 +0300

    Playground

---
+++ src/all.js
@@ -0,0 +1,22 @@
+// How to generate: run import("./generate-imports-for-all.js") from index.html, copy output.
+
+import countDeclarations from "./countDeclarations.js";
+export {countDeclarations};
+
+import countProperties from "./countProperties.js";
+export {countProperties};
+
+import matches from "./matches.js";
+export {matches};
+
+import sortObject from "./sortObject.js";
+export {sortObject};
+
+import sumObject from "./sumObject.js";
+export {sumObject};
+
+import walkDeclarations from "./walkDeclarations.js";
+export {walkDeclarations};
+
+import walkRules from "./walkRules.js";
+export {walkRules};
